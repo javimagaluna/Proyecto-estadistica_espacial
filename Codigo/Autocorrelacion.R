32b99594dd7f1c20e018ba32e2db1133b0bdeedc
paulamparada <53879836+Patodinosaurio@users.noreply.github.com>
2021-12-16 04:22:11 -0300

    Actualiazado autocorrelaciones

--- Codigo/Autocorrelacion.R
+++ Codigo/Autocorrelacion.R
@@ -9,6 +9,8 @@ library(tmap)
 library(readr) 
 library(readxl)
 library(spdep)
+library(spatialreg)
+
 
 
 # Bases -------------------------------------------------------------------
@@ -34,7 +36,19 @@ D2017 <- RM %>%
 D2017 <- D2017 %>% 
   mutate(Pob_Viv = POB/VIV)
 
+# Base delitos
 
+Del <- readxl::read_excel("Datos/Delitos_todos.xlsx")
+
+# Arreglar el nombre de la comuna
+Del$COMUNA[Del$COMUNA == "Caracaví"] <- "Curacaví"
+
+D2017_Del <- D2017 %>% 
+  mutate(COMUNA = tolower(D2017$COMUNA))
+
+D2017_Del <- D2017 %>% 
+  left_join(Del)
+  
 # Autocorrelación ---------------------------------------------------------
 
 # Defino los vecinos
@@ -84,15 +98,16 @@ mapview::mapview(D2017["HACI"],
 ### Indice 
 moran.test(D2017$Pob_Viv, b_pesos, zero.policy = TRUE)
 moran.plot(D2017$Pob_Viv, b_pesos, zero.policy = TRUE,
-           ylab = "Spatially Lagged Hacinamiento",
+           ylab = "Spatially Lagged Hab por Viv",
            xlab = "% Hacinamiento",
            main = "Moran Test")
 
 moran.test(D2017$Pob_Viv, w_pesos, zero.policy = TRUE)
 moran.plot(D2017$Pob_Viv, w_pesos, zero.policy = TRUE,
-           ylab = "Spatially Lagged Hacinamiento",
+           ylab = "Spatially Lagged Hab por Viv",
            xlab = "% Hacinamiento",
            main = "Moran Test")
+?moran.plot
 
 geary.test(D2017$Pob_Viv, b_pesos, zero.policy = TRUE)
 geary.test(D2017$Pob_Viv, w_pesos, zero.policy = TRUE)
@@ -115,3 +130,219 @@ mapview::mapview(D2017["Pob_Viv"],
                    , D2017$Pob_Viv
                  ))
 
+### Indice MASCU
+moran.test(D2017$MASCU, b_pesos, zero.policy = TRUE)
+geary.test(D2017$MASCU, b_pesos, zero.policy = TRUE)
+mapview::mapview(D2017["MASCU"],
+                 layer.name = "Índice Masculinidad",
+                 legend = TRUE,
+                 popup = paste0(
+                   "<b>COMUNA: </b>"
+                   , D2017$COMUNA
+                   , "<br><b>Índice Masculinidad: </b>"
+                   , D2017$Pob_Viv
+                 ))
+
+### Escolaridad
+moran.test(D2017$ESC, b_pesos, zero.policy = TRUE)
+geary.test(D2017$ESC, b_pesos, zero.policy = TRUE)
+mapview::mapview(D2017["ESC"],
+                 layer.name = "Escolaridad",
+                 legend = TRUE,
+                 popup = paste0(
+                   "<b>COMUNA: </b>"
+                   , D2017$COMUNA
+                   , "<br><b>Escolaridad: </b>"
+                   , D2017$Pob_Viv
+                 ))
+
+### Delito VIO.MUJ
+
+unique(D2017_Del$ANIO)
+## 2010
+D2017_Del_2010 <- D2017_Del %>% 
+  filter(ANIO == 2010)
+
+moran.test(D2017_Del_2010$VIO.MUJ, b_pesos, zero.policy = TRUE) 
+moran.test(D2017_Del_2010$VIO.MUJ, w_pesos, zero.policy = TRUE)
+
+geary.test(D2017_Del_2010$VIO.MUJ, b_pesos, zero.policy = TRUE)
+geary.test(D2017_Del_2010$VIO.MUJ, w_pesos, zero.policy = TRUE)
+
+### 2011
+D2017_Del_2011 <- D2017_Del %>% 
+  filter(ANIO == 2011)
+
+moran.test(D2017_Del_2011$VIO.MUJ, b_pesos, zero.policy = TRUE) # A 0.1
+moran.test(D2017_Del_2011$VIO.MUJ, w_pesos, zero.policy = TRUE)
+
+geary.test(D2017_Del_2011$VIO.MUJ, b_pesos, zero.policy = TRUE)
+geary.test(D2017_Del_2011$VIO.MUJ, w_pesos, zero.policy = TRUE)
+
+### Delito VIO.NIN
+## 2010
+moran.test(D2017_Del_2010$VIO.NIN, b_pesos, zero.policy = TRUE)
+moran.test(D2017_Del_2010$VIO.NIN, w_pesos, zero.policy = TRUE)
+
+geary.test(D2017_Del_2010$VIO.NIN, b_pesos, zero.policy = TRUE)
+geary.test(D2017_Del_2010$VIO.NIN, w_pesos, zero.policy = TRUE)
+
+### 2011
+moran.test(D2017_Del_2011$VIO.MUJ, b_pesos, zero.policy = TRUE) # A 0.1
+moran.test(D2017_Del_2011$VIO.MUJ, w_pesos, zero.policy = TRUE)
+
+geary.test(D2017_Del_2011$VIO.MUJ, b_pesos, zero.policy = TRUE)
+geary.test(D2017_Del_2011$VIO.MUJ, w_pesos, zero.policy = TRUE)
+
+### Delito VIO.ADUlT
+## 2010
+moran.test(D2017_Del_2010$VIO.ADULT, b_pesos, zero.policy = TRUE)
+moran.test(D2017_Del_2010$VIO.ADULT, w_pesos, zero.policy = TRUE)
+
+geary.test(D2017_Del_2010$VIO.ADULT, b_pesos, zero.policy = TRUE)
+geary.test(D2017_Del_2010$VIO.ADULT, w_pesos, zero.policy = TRUE)
+
+### 2011
+moran.test(D2017_Del_2011$VIO.ADULT, b_pesos, zero.policy = TRUE)
+moran.test(D2017_Del_2011$VIO.ADULT, w_pesos, zero.policy = TRUE)
+
+geary.test(D2017_Del_2011$VIO.ADULT, b_pesos, zero.policy = TRUE)
+geary.test(D2017_Del_2011$VIO.ADULT, w_pesos, zero.policy = TRUE)
+
+### Delito ALCOHOL
+## 2010
+moran.test(D2017_Del_2010$ALCOHOL, b_pesos, zero.policy = TRUE)
+moran.test(D2017_Del_2010$ALCOHOL, w_pesos, zero.policy = TRUE)
+
+geary.test(D2017_Del_2010$ALCOHOL, b_pesos, zero.policy = TRUE)
+geary.test(D2017_Del_2010$ALCOHOL, w_pesos, zero.policy = TRUE)
+
+
+### Delito ABUSO
+## 2010
+moran.test(D2017_Del_2010$ABUSO, b_pesos, zero.policy = TRUE) # Este sí
+moran.test(D2017_Del_2010$ABUSO, w_pesos, zero.policy = TRUE) # A 0.1
+
+geary.test(D2017_Del_2010$ABUSO, b_pesos, zero.policy = TRUE)
+geary.test(D2017_Del_2010$ABUSO, w_pesos, zero.policy = TRUE)
+
+### 2011
+moran.test(D2017_Del_2011$ABUSO, b_pesos, zero.policy = TRUE) # A 0.1
+moran.test(D2017_Del_2011$ABUSO, w_pesos, zero.policy = TRUE)
+
+geary.test(D2017_Del_2011$VIO.MUJ, b_pesos, zero.policy = TRUE)
+geary.test(D2017_Del_2011$VIO.MUJ, w_pesos, zero.policy = TRUE)
+
+### Delito RUIDO
+## 2010
+moran.test(D2017_Del_2010$RUIDO, b_pesos, zero.policy = TRUE)
+moran.test(D2017_Del_2010$RUIDO, w_pesos, zero.policy = TRUE)
+
+geary.test(D2017_Del_2010RUIDO, b_pesos, zero.policy = TRUE)
+geary.test(D2017_Del_2010$RUIDO, w_pesos, zero.policy = TRUE)
+
+### 2011
+moran.test(D2017_Del_2011$RUIDO, b_pesos, zero.policy = TRUE)
+moran.test(D2017_Del_2011$RUIDO, w_pesos, zero.policy = TRUE)
+
+geary.test(D2017_Del_2011$RUIDO, b_pesos, zero.policy = TRUE)
+geary.test(D2017_Del_2011$RUIDO, w_pesos, zero.policy = TRUE)
+
+# Modelo ------------------------------------------------------------------
+
+# Defino los vecinos
+vecinos2 <- poly2nb(D2017_Del)
+
+# Hago las matrices de pesos
+b_pesos2 <- nb2listw(vecinos2, style ="B", zero.policy = TRUE)
+w_pesos2 <- nb2listw(vecinos2, style ="W", zero.policy = TRUE)
+
+ver_b_pesos2 <- listw2mat(b_pesos2)
+ver_w_pesos2 <- listw2mat(w_pesos2)
+
+## MODELO LINEAL
+mod_lin <- lm(ABUSO ~ HACI + MASCU + ESC + Pob_Viv, data = D2017_Del)
+summary(mod_lin)
+AIC(mod_lin)
+plot(residuals(mod_lin))
+
+## MODELO CAR
+
+mod_CAR <- spautolm(ABUSO ~ HACI + MASCU + ESC + Pob_Viv, data = D2017_Del, listw = b_pesos2, family = "CAR")
+summary(mod_CAR)
+shapiro.test(residuals(mod_CAR))
+plot(residuals(mod_CAR))
+
+## MODELO SAR
+
+mod_SAR <- spautolm(ABUSO ~ HACI + MASCU + ESC + Pob_Viv, data = D2017_Del, listw = b_pesos2, family = "SAR")
+summary(mod_SAR)
+plot(residuals(mod_SAR))
+
+###
+
+## MODELO LINEAL
+mod_lin <- lm(ABUSO ~ HACI + ESC + Pob_Viv, data = D2017_Del)
+summary(mod_lin)
+AIC(mod_lin)
+plot(residuals(mod_lin))
+
+## MODELO CAR
+
+mod_CAR <- spautolm(ABUSO ~ HACI + ESC + Pob_Viv, data = D2017_Del, listw = b_pesos2, family = "CAR")
+summary(mod_CAR)
+plot(residuals(mod_CAR))
+shapiro.test(residuals(mod_CAR))
+
+## MODELO SAR
+
+mod_SAR <- spautolm(ABUSO ~ HACI + ESC + Pob_Viv, data = D2017_Del, listw = b_pesos2, family = "SAR")
+summary(mod_SAR)
+plot(residuals(mod_SAR))
+
+# MODELO CAR TIENE MENOR AIC, TODOS AL SACAR MASC DISMINUYEN AIC
+
+## MODELO LINEAL
+mod_lin <- lm(ABUSO ~ HACI + Pob_Viv, data = D2017_Del)
+summary(mod_lin)
+AIC(mod_lin)
+plot(residuals(mod_lin))
+
+## MODELO CAR
+
+mod_CAR <- spautolm(ABUSO ~ HACI + Pob_Viv, data = D2017_Del, listw = b_pesos2, family = "CAR")
+summary(mod_CAR)
+plot(residuals(mod_CAR))
+shapiro.test(residuals(mod_CAR))
+
+## MODELO SAR
+
+mod_SAR <- spautolm(ABUSO ~ HACI + Pob_Viv, data = D2017_Del, listw = b_pesos2, family = "SAR")
+summary(mod_SAR)
+plot(residuals(mod_SAR))
+
+# MODELO CAR ES EL MEJOR, LM AUMENTA AL SACAR ESC
+
+## MODELO LINEAL
+mod_lin <- lm(ABUSO ~ HACI, data = D2017_Del)
+summary(mod_lin)
+AIC(mod_lin)
+plot(residuals(mod_lin))
+
+
+## MODELO CAR
+
+mod_CAR <- spautolm(ABUSO ~ HACI, data = D2017_Del, listw = b_pesos2, family = "CAR")
+summary(mod_CAR)
+plot(residuals(mod_CAR))
+shapiro.test(residuals(mod_CAR))
+?shapiro.test
+
+
+## MODELO SAR
+
+mod_SAR <- spautolm(ABUSO ~ HACI, data = D2017_Del, listw = b_pesos2, family = "SAR")
+summary(mod_SAR)
+plot(residuals(mod_SAR))
+
+# MODELO CAR ES EL MEJOR, TODOS AUMENTAN AL SACAR Pob_Viv
